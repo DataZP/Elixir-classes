9e35ffeff080138244730ddf595862567b06bdf2
Nicolas Zapata <nicolas.zapata@dinmax.com>
2020-03-30 09:52:29 -0300

    First commit, elixir Hello World

---
+++ helloworld/lib/helloworld.ex
@@ -0,0 +1,18 @@
+defmodule Helloworld do
+  @moduledoc """
+  This is a module for a Hello World
+  """
+
+  @doc """
+  When you print it says hello to the world!
+
+  ## Examples
+
+      iex> helloworld.hello()
+      :ok
+
+  """
+  def hello() do
+    "Hello World!"
+  end
+end
